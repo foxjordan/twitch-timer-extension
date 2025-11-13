1887de9930721088c6985576bc48ce9960f3aa77
Jordan Fox <fox_jordan@live.com>
2025-11-13 07:45:13 -0800

    .

--- ebs/newrelic.cjs
+++ ebs/newrelic.cjs
@@ -1,14 +1,9 @@
 /*
- * New Relic agent configuration.
+ * New Relic agent configuration (CommonJS).
  * See: https://docs.newrelic.com/docs/apm/agents/nodejs-agent/configuration/nodejs-agent-configuration/
  */
 'use strict'
 
-/**
- * NOTE: This config file is CommonJS. It is loaded by Node via `-r newrelic`.
- * Values may be overridden by environment variables. Common ones:
- *   NEW_RELIC_LICENSE_KEY, NEW_RELIC_APP_NAME, NEW_RELIC_LOG_LEVEL
- */
 exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME || 'twitch-timer-ebs'],
   license_key: process.env.NEW_RELIC_LICENSE_KEY || '',
@@ -35,8 +30,6 @@ exports.config = {
     record_sql: 'obfuscated'
   },
   browser_monitoring: { enabled: false },
-  rules: {
-    ignore: [/^\/healthz$/]
-  }
+  rules: { ignore: [/^\/healthz$/] }
 }
 
